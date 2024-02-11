a323bbfc78d47d289d70b16cc9bf83a5543b76bf
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-02-12 01:19:36 +0330

    Update Gost.sh

--- Gost.sh
+++ Gost.sh
@@ -107,7 +107,7 @@ fi
     wget https://github.com/go-gost/gost/releases/download/v3.0.0-nightly.20240128/gost_3.0.0-nightly.20240128_linux_amd64.tar.gz && \
     echo $'\e[32mGost downloaded successfully.\e[0m' && \
     tar -xvzf gost_3.0.0-nightly.20240128_linux_amd64.tar.gz -C /usr/local/bin/ && \
-    cd /usr/local/bin/ && chmod +x /usr/local/bin/gost && \ 
+    chmod +x /usr/local/bin/gost && \ 
     echo $'\e[32mGost installed successfully.\e[0m'
 else
     echo $'\e[31mInvalid choice. Exiting...\e[0m'
