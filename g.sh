6d28d5e520f6def0d7467492953987ce4ab921af
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-02-27 00:16:51 +0330

    Update g.sh

--- g.sh
+++ g.sh
@@ -10,7 +10,7 @@ temp_dir=$(mktemp -d)
 cd "$temp_dir" || exit
 wget https://github.com/masoudgb/Gost-ip6/raw/main/install.sh
 sudo mkdir -p /etc/gost
-sudo mv Gost.sh /etc/gost/
+sudo mv install.sh /etc/gost/
 sudo chmod +x /etc/gost/install.sh
 cd /etc/gost || exit
 sudo ./install.sh
