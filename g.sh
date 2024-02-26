14862e819bc40c149bea6eb1339c2cf06cc10322
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-02-27 00:28:49 +0330

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Check if the Gost script is already present in the /etc/gost directory
-if [ -f "/etc/gost/install.sh" ]; then
-    echo $'\e[32mThe Gost script has been previously downloaded. No repeated download.\e[0m'
-else
-
 # Download the Gost script from GitHub
 temp_dir=$(mktemp -d)
 cd "$temp_dir" || exit
