8d138c61d629693287523c3851c0fdd04b667634
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-02-27 19:33:40 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -429,9 +429,11 @@ sudo rm -rf /etc/gost
 sudo rm -f /usr/lib/systemd/system/gost_*.service
 sudo rm -f /etc/systemd/system/multi-user.target.wants/gost_*.service
 # Remove the command from sysctl.conf if it exists
-if grep -q "net.ipv4.ip_local_port_range=\1024 65535\" /etc/sysctl.conf; then
-    sudo sed -i '/net.ipv4.ip_local_port_range=1024 65535/d' /etc/sysctl.conf
-    echo $'\e[32mCommand removed from sysctl.conf.\e[0m'
+if grep -qE '^\s*net.ipv4.ip_local_port_range\s*=\s*1024 65535' /etc/sysctl.conf; then
+    sudo sed -i '/net.ipv4.ip_local_port_range\s*=\s*1024 65535/d' /etc/sysctl.conf
+    sudo sysctl -p
+else
+    echo "The specified line does not exist in /etc/sysctl.conf."
 fi
 echo $'\e[32mGost successfully uninstalled.\e[0m'
 
