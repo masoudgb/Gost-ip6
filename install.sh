c8a167a5887bb9cd8d55567bb95fbb40d8f57555
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-02-27 19:45:36 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -422,21 +422,20 @@ elif [ "$choice" -eq 9 ]; then
             crontab -l | grep -v '/usr/bin/auto_restart_cronjob.sh' | crontab -
 
             # Continue with the rest of the uninstallation process
-sudo systemctl daemon-reload
-sudo systemctl stop gost_*.service
-sudo rm -f /usr/local/bin/gost
-sudo rm -rf /etc/gost
-sudo rm -f /usr/lib/systemd/system/gost_*.service
-sudo rm -f /etc/systemd/system/multi-user.target.wants/gost_*.service
-# Remove the command from sysctl.conf if it exists
-if grep -qE '^\s*net.ipv4.ip_local_port_range\s*=\s*1024 65535' /etc/sysctl.conf; then
-    sudo sed -i '/net.ipv4.ip_local_port_range\s*=\s*1024 65535/d' /etc/sysctl.conf
-    sudo sysctl -p
-else
-    echo "The specified line does not exist in /etc/sysctl.conf."
-fi
-echo $'\e[32mGost successfully uninstalled.\e[0m'
-
+            sudo systemctl daemon-reload
+            sudo systemctl stop gost_*.service
+            sudo rm -f /usr/local/bin/gost
+            sudo rm -rf /etc/gost
+            sudo rm -f /usr/lib/systemd/system/gost_*.service
+            sudo rm -f /etc/systemd/system/multi-user.target.wants/gost_*.service
+            # Remove the command from sysctl.conf if it exists
+            if grep -qE '^\s*net.ipv4.ip_local_port_range\s*=\s*1024 65535' /etc/sysctl.conf; then
+                sudo sed -i '/net.ipv4.ip_local_port_range\s*=\s*1024 65535/d' /etc/sysctl.conf
+                sudo sysctl -p
+            else
+                echo "The specified line does not exist in /etc/sysctl.conf."
+            fi
+            echo $'\e[32mGost successfully uninstalled.\e[0m'
         }
     else
         echo $'\e[32mUninstallation canceled.\e[0m'
