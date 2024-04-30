a7fca9c6a6c5a1c9df45a73e249e33677fa91763
Masoud Gb <87688187+masoudgb@users.noreply.github.com>
2024-04-30 11:24:26 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo $'\e[35m'"  ___|              |        _ _|  _ \   /
                                               "$'\e[0m'
 
 echo -e "\e[36mCreated By Masoud Gb Special Thanks Hamid Router\e[0m"
-echo $'\e[35m'"Gost Ip6 Script v2.1.7"$'\e[0m'
+echo $'\e[35m'"Gost Ip6 Script v2.2.0"$'\e[0m'
 
 options=($'\e[36m1. \e[0mGost Tunnel By IP4'
          $'\e[36m2. \e[0mGost Tunnel By IP6'
